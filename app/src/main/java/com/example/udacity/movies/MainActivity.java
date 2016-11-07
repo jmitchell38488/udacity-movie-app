fc28e316df4c7e9726d47c205e3a45fa40ca9cb6
Justin Mitchell <jmitchell38488@gmail.com>
2016-11-07 11:51:14 +1100

    Adding example code from the Android ImageLoading into GridView

--- app/src/main/java/com/example/udacity/movies/MainActivity.java
+++ app/src/main/java/com/example/udacity/movies/MainActivity.java
@@ -8,6 +8,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import com.example.udacity.movies.adapter.ImageAdapter;
+import com.example.udacity.movies.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,17 +21,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // Add TODAY fragment to main
+        if (savedInstanceState == null) {
+            MainFragment fragment = new MainFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.main_container, fragment)
+                    .commit();
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/example/udacity/movies/fragment/MainFragment.java
@@ -0,0 +1,55 @@
+package com.example.udacity.movies.fragment;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import com.example.udacity.movies.R;
+import com.example.udacity.movies.adapter.ImageAdapter;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainFragment extends Fragment {
+
+    private ImageAdapter mAdapter;
+
+    public MainFragment() {
+        // do nothing
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(false);
+
+        mAdapter = new ImageAdapter(getActivity());
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        GridView gridview = (GridView) getActivity().findViewById(R.id.movies_grid);
+
+        ImageAdapter mAdapter = new ImageAdapter(getActivity());
+        gridview.setAdapter(mAdapter);
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(getActivity(), "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
